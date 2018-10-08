e8b9b430e7f5b22a5c547ed6ab358eb44a0e5c66
Amy <amyemoretti@gmail.com>
2018-10-08 17:00:07 -0500

    star updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-import Favorite from './component/Favorite'
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Favorite/>
+      
       </div>
     );
   }
