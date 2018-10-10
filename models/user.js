ec49e08129fdf0343656f25c5b106e8b7d2b5341
bobbiro2e <bobbiro2e@gmail.com>
2018-10-10 15:23:55 -0500

    fav model

--- models/user.js
+++ models/user.js
@@ -3,9 +3,11 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new Schema({
-    username: {type: String},
-    password: {type: String},
-    trips:[{type: Schema.Types.ObjectId, ref: 'Trip'}]
+    username: { type: String },
+    password: { type: String },
+    trips: [{ type: Schema.Types.ObjectId, ref: 'Trip' }],
+    favtrips: [{ type: Schema.Types.ObjectId, ref: 'Trip' }]
+
 });
 
 UserSchema.plugin(passportLocalMongoose);
