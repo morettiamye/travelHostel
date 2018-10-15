7515234a00626a26e5ba8ab66fd147af967162b6
Elephant_Man <grogan.charlie@gmail.com>
2018-10-15 11:34:25 -0500

    trip card working

--- client/src/component/Modal/Modal.jsx
+++ client/src/component/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Favorite from "../Favorite";
 import "./modal.css";
 
-const Modal = ({ children, closeModal, modalState, title }) => {
+const Modal = ({ closeModal, modalState, trip }) => {
   if (!modalState) {
     return null;
   }
@@ -17,11 +17,30 @@ const Modal = ({ children, closeModal, modalState, title }) => {
       <div className="modal-background" onClick={closeModal} />
       <div className="modal-card">
         <header className="modal-card-head head">
-          <p className="modal-card-title">{title}</p>
+          <p className="modal-card-title">{trip.title}</p>
           <button className="delete" onClick={closeModal} />
         </header>
         <section className="modal-card-body details">
-          <div className="content">{children}</div>
+          <div className="content">
+            <p>{`City Explored : 
+            ${trip.city}`}</p>
+            <p>{`Time Spent Exploring : 
+            ${trip.duration} days`}</p>
+            <p>{`Season Traveled : 
+            ${trip.season}`}</p>
+            <p>{`How we got to there : 
+            ${trip.transportations}`}</p>
+            <p>{`Our accomodations : 
+            ${trip.accomodations}`}</p>
+            <p>{`Some food spots I recommend from my trip : 
+            ${trip.food}`}</p>
+            <p>{`Some activities I highly suggest you try : 
+            ${trip.activities}`}</p>
+            <div>
+              <p>Check out these awesome photos I took!</p>
+              <img src={trip.photos} alt="trip photos" />
+            </div>
+          </div>
         </section>
         <footer className="modal-card-foot foot">
           <a className="button" onClick={closeModal}>
